a0bfa71399ce416873289f92c8f4f9c2edf2b98a
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2017-10-18 22:52:34 +1300

    What: Refactor Asset Bundle logic
    Changes:
    * Rename Asset::Bundler class to AssetBundle
    * Rename bundler file to asset_bundle
    * Move file zip functionality to AssetBundle class form Nod helpers
    * Require asset_bundle file rather than bundler
    * Update Nod bundle CLI method to use AssetVundle implementation

--- lib/nod/helpers.rb
+++ lib/nod/helpers.rb
@@ -1,14 +1,6 @@
 module Nod
   # lib/nod/helpers.rb
   module Helpers
-    def zip_assets(files)
-      file_path = "#{@name}_assets.zip"
-      Zip::File.open(file_path, Zip::File::CREATE) do |zipfile|
-        files.each do |file|
-          zipfile.add(::File.basename(file), file)
-        end
-      end
-    end
 
     def asset_exists?(name)
       Dir.new(Dir.pwd).each do |asset|
