cff630c4790290747ebc8b464d1f10328cdfe586
Theo Paul <theo.markkus.paul@gmail.com>
2017-10-20 14:42:15 +1300

    Make helper methods both class and instance methods

--- lib/nod/helpers.rb
+++ lib/nod/helpers.rb
@@ -2,6 +2,11 @@ module Nod
   # lib/nod/helpers.rb
   module Helpers
 
+    # make helper methods, class methods
+    def Helpers.included(mod)
+      mod.extend Helpers
+    end
+
     def asset_exists?(name)
       Dir.new(Dir.pwd).each do |asset|
         return true if asset == name
