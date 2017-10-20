83677fd4b6ba2caae6da0f2fd97ba355373c3cb0
Theo Paul <theo.markkus.paul@gmail.com>
2017-10-20 19:15:12 +1300

    Changes: * Typo fixes * Add asset files as an accessible attribute * Add #list_files as an instance method * Use asset_files instance variable implementation over file_paths local var in #bundle method * Add specs for #list_files * Add spec for @asset_files instance variable * Add specs for #bundle method

--- spec/nod/asset_bundle_spec.rb
+++ spec/nod/asset_bundle_spec.rb
@@ -9,8 +9,33 @@ RSpec.describe Nod::AssetBundle do
     it 'sets the name attribute' do
       expect(asset_bundle.new(project_name).name).to eql project_name
     end
+    it 'sets an empty array for asset files as an attribute' do
+      new_asset_bundle = asset_bundle.new(project_name)
+      expect(new_asset_bundle.asset_files).to be_a Array
+      expect(new_asset_bundle.asset_files.empty?).to be true
+    end
     it 'returns an AssetBundle object' do
       expect(asset_bundle.new(project_name)).to be_a asset_bundle
+    end
+  end
+
+  describe '#bundle' do
+    let(:bundled_obj) { asset_bundle.new(project_name) }
+    it 'raises an error if the asset does not exist' do
+      expect { bundled_obj.bundle }.to raise_error 'Asset doesn\'t exist'
+    end
+    it 'returns a collection of zipped files' do
+      # mock fake asset to exist
+      allow_any_instance_of(asset_bundle).to receive(:asset_exists?).and_return(true)
+
+      # mock writing to file
+      allow(::File).to receive(:write).and_return(true)
+
+      # mock zipping to file
+      files = ['index.html', 'style.css', 'main.js']
+      allow(Zip::File).to receive(:open).and_return(files)
+
+      expect(bundled_obj.bundle).to eql files
     end
   end
 
@@ -54,6 +79,32 @@ RSpec.describe Nod::AssetBundle do
         # be_truthy is a bit misleading
         expect(bundle.file).to be_truthy
       end
+    end
+  end
+
+  describe '#list_files' do
+    let(:files) { ['index.html', 'style.css', 'main.js'] }
+    let(:bundled_obj) do
+      asset             = asset_bundle.new('bundle')
+      asset.file_path   = '/bundle.zip'
+      asset.file        = 'data in the file'
+      asset.asset_files = files
+      asset
+    end
+
+    before do
+      # mock AssetBundle class find method
+      allow(asset_bundle).to receive(:find).and_return(bundled_obj)
+
+      @bundle = asset_bundle.find(project_name)
+    end
+    it 'returns a collection of all files in the asset bundle' do
+      expect(@bundle.list_files).to eql files
+    end
+    it 'returns an empty collection if the asset bundle is empty' do
+      @bundle.asset_files = []
+
+      expect(@bundle.list_files).to eql []
     end
   end
 end
